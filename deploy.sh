9f04a8f7fb37d782d03fff8720a57fdf54d020bd
Алексей Довбня <i-zerg_ec@mail.ru>
2017-12-09 20:24:55 +0300

    deploy edit

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ zip -r build.zip build
 curl -H "Content-type: application/zip" \
      -H "Authorization: Bearer $NETLIFY_KEY" \
      --data-binary "@build.zip" \
-https://api.netlify.com/api/v1/sites/
+https://api.netlify.com/api/v1/sites/59b5d6e9-8326-432a-808c-be5ed480bdd6
